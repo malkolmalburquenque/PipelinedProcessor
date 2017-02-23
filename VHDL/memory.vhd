bf4f64df3f9ae6c7861cf5609d49443a5953280c
Malkolm Alburquenque <malkolm.alburquenque@mail.mcgill.ca>
2017-02-23 13:47:46 -0500

    memory.vhd updated from email instructions

--- VHDL/memory.vhd
+++ VHDL/memory.vhd
@@ -5,23 +5,23 @@ USE ieee.numeric_std.all;
 
 ENTITY memory IS
 	GENERIC(
-		ram_size : INTEGER := 32768;
-		mem_delay : time := 10 ns;
+		ram_size : INTEGER := 8192;
+		mem_delay : time := 1 ns;
 		clock_period : time := 1 ns
 	);
 	PORT (
 		clock: IN STD_LOGIC;
-		writedata: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+		writedata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
 		address: IN INTEGER RANGE 0 TO ram_size-1;
 		memwrite: IN STD_LOGIC;
 		memread: IN STD_LOGIC;
-		readdata: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+		readdata: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
 		waitrequest: OUT STD_LOGIC
 	);
 END memory;
 
 ARCHITECTURE rtl OF memory IS
-	TYPE MEM IS ARRAY(ram_size-1 downto 0) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
+	TYPE MEM IS ARRAY(ram_size-1 downto 0) OF STD_LOGIC_VECTOR(31 DOWNTO 0);
 	SIGNAL ram_block: MEM;
 	SIGNAL read_address_reg: INTEGER RANGE 0 to ram_size-1;
 	SIGNAL write_waitreq_reg: STD_LOGIC := '1';
