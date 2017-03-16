d9075f6cb074d2265223fa2aec0d6b7d4a395e39
Joel Lat <Joel Lat>
2017-03-15 21:01:19 -0400

    fixed wb and mem

--- VHDL/mem.vhd
+++ VHDL/mem.vhd
@@ -3,74 +3,81 @@ use IEEE.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity mem is
-port (ctrl_write : in std_logic;
+GENERIC(
+	ram_size : INTEGER := 8192; 
+	mem_delay : time := 10 ns;
+	clock_period : time := 1 ns
+);
+port (clk: in std_logic;
+	-- Control lines
+	ctrl_write : in std_logic;
 	ctrl_read: in std_logic;
 	ctrl_memtoreg_in: in std_logic;
 	ctrl_memtoreg_out: out std_logic;
 	ctrl_regwrite_in: in std_logic;
 	ctrl_regwrite_out: out std_logic;
+
+	--Ports of stage
 	alu_in : in std_logic_vector (31 downto 0);
+	alu_out : out std_logic_vector (31 downto 0);
 	mem_data_in: in std_logic_vector (31 downto 0);
-	mem_data_out: out std_logic (31 downto 0);
-	write_addr_in: in std_logic_vector (7 downto 0);
-	write_addr_out: out std_logic_vector (7 downto 0);
+	mem_data_out: out std_logic_vector (31 downto 0);
+	write_addr_in: in std_logic_vector (4 downto 0);
+	write_addr_out: out std_logic_vector (4 downto 0);
+	
+	--Memory signals
+	writedata: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+	address: OUT INTEGER RANGE 0 TO ram_size-1;
+	memwrite: OUT STD_LOGIC := '0';
+	memread: OUT STD_LOGIC := '0';
+	readdata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+	waitrequest: IN STD_LOGIC
+	
   );
-end pipeline;
+end mem;
 
 architecture behavioral of mem is
 
-COMPONENT memory IS
-        GENERIC(
-            ram_size : INTEGER := 8192;
-            mem_delay : time := 10 ns;
-            clock_period : time := 1 ns
-        );
-        PORT (
-            clock: IN STD_LOGIC;
-            writedata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-            address: IN INTEGER RANGE 0 TO ram_size-1;
-            memwrite: IN STD_LOGIC := '0';
-            memread: IN STD_LOGIC := '0';
-            readdata: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-            waitrequest: OUT STD_LOGIC
-        );
-    END COMPONENT;
 
-signal mem_data_next: std_logic_vector (31 downto 0);
-signal write_addr_next: std_logic_vector (7 downto 0);
+signal mem_data_next, alu_next: std_logic_vector (31 downto 0);
+signal write_addr_next: std_logic_vector (4 downto 0);
 signal ctrl_memtoreg_next, ctrl_regwrite_next: std_logic;
 			
 begin
+
 process (clk)
 begin
 	if (clk'event and clk = '1') then
-	write_addr_out <= write_addr_next;
-	ctrl_regwrite_out <= ctrl_memtoreg_next;
-	mem_data_out <= mem_data_next;
-	ctrl_memtoreg_out <= ctrl_memtoreg_next;
-	memwrite <= '0';
-	memread <= '0';
+		write_addr_out <= write_addr_next;
+		mem_data_out <= mem_data_next;
+		alu_out <= alu_next;
+		
+		
+		ctrl_regwrite_out <= ctrl_memtoreg_next;
+		ctrl_memtoreg_out <= ctrl_memtoreg_next;
+		memwrite <= '0';
+		memread <= '0';
 	end if;
 end process;
 
-write_addr_next <= write_addr_in;
-ctrl_memtoreg_next <= ctrl_memtoreg_in;
-ctrl_regwrite_next <= ctrl_regwrite_in;
-alu_next <= alu_in;
 
---Assume fast enough
-address <= alu_in;
-	
---IF STORE
-if (ctrl_write == "1"){
-	memwrite <= '1';
-	writedata <= mem_data_in;
-}
---IF LOAD
-else if (ctrl_read == "1"){
-	memread <= '1'
-	mem_next <= readdata
-}
+process (write_addr_in , ctrl_memtoreg_in, ctrl_regwrite_in, alu_in)
+begin
+	--Propogate signals
+	write_addr_next <= write_addr_in;
+	ctrl_memtoreg_next <= ctrl_memtoreg_in;
+	ctrl_regwrite_next <= ctrl_regwrite_in;
+	alu_next <= alu_in;
 
+	--Access memory
+	address <= to_integer(unsigned(alu_in));	
+	if ctrl_write = '1' then
+		memwrite <= '1';
+		writedata <= mem_data_in;
+	elsif ctrl_read = '1' then
+		memread <= '1';
+		mem_data_next <= readdata;
+	end if;
+end process;
 
 end behavioral;
--- VHDL/wb.vhd
+++ VHDL/wb.vhd
@@ -3,32 +3,36 @@ use IEEE.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity wb is
-port (alu_in : in std_logic_vector (31 downto 0);
-	ctrl_memtoreg : in std_logic ;
-	ctrl_regwrite : in std_logic;
-	mem_in: in std_logic (31 downto 0);
+port (ctrl_memtoreg_in: in std_logic;
+	ctrl_regwrite_in: in std_logic;
+	ctrl_regwrite_out: out std_logic;
+	
+	alu_in : in std_logic_vector (31 downto 0);
+	mem_in: in std_logic_vector (31 downto 0);
 	mux_out : out std_logic_vector (31 downto 0);
-	write_addr_out: out std_logic_vector (7 downto 0);
-	write_enabled: out std_logic;
+	write_addr_in: in std_logic_vector (7 downto 0);
+	write_addr_out: out std_logic_vector (7 downto 0)
   );
-end pipeline;
+end wb;
 
 architecture behavioral of wb is
 
 begin
+process(alu_in, mem_in, ctrl_memtoreg_in, ctrl_regwrite_in)
+begin
+	write_addr_out <= write_addr_in;
+	ctrl_regwrite_out <= ctrl_regwrite_in;
 
-write_addr_out <= write_addr_in;
-write_enabled <= ctrl_regwrite;
-case ctrl_memtoreg is
-	--ALU
-	when "0" => 
-		mux_out <= alu_in;
-		 <= '1';
-	--LOAD
-	when "1" => 
-		mux_out <= mem_in;
-	when others => 
-		mux_out <= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-end case;
+	case ctrl_memtoreg_in is
+		--ALU
+		when '0' => 
+			mux_out <= alu_in;
+		--MEM
+		when '1' => 
+			mux_out <= mem_in;
+		when others => 
+			mux_out <= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+	end case;
+end process;
 
 end behavioral;
